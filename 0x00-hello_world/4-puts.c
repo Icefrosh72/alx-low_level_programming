25d7df6a28d97f95404e63296938eb7c3037f1d2
Icefrosh72 <seidutesleem06@gmail.com>
2022-09-10 03:47:33 -0700

    task put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main- print the string in the blank function
+*
+* Description- using the main function
+* must print "Programming is like building a multilingual puzzle
+* Return 0
+*/
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	reurn (0);
