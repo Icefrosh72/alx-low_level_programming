f51ad7fcfda00a47a6aebdb1418867e06bb222e8
Icefrosh72 <seidutesleem06@gmail.com>
2022-09-12 03:45:34 -0700

    prints number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,8 +8,7 @@
  */
 int main(void)
 {
-	int n;
-	
+	int n;	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -24,5 +23,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
