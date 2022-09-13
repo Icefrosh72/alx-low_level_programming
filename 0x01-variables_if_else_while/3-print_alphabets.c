b920852d8067791e2e98799ebfdc77c39ebb60dd
Icefrosh72 <seidutesleem06@gmail.com>
2022-09-13 03:51:13 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#incldue <stdio.h>
+/**
+ * main - printif the numbeer is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this prints "Programming is positve,zero ,or negative
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
